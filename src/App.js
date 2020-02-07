f97b4e5f5ba5d59ec0323fb27d5a05ebf096a1bb
DanielKim <dltcm0827@gmail.com>
2020-02-06 22:48:40 -0800

    added data in App Component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import React, { Component } from "react";
 import PhoneForm from "./components/PhoneForm";
 
 class App extends Component {
-  handleCreate = data => {
-    console.log(data);
+  // Never use. (push, splice, unshift, pop) mutates
+  // Better use. (concat, slice, map, filter) do not mutate
+  id = 2;
+  state = {
+    information: [
+      {
+        id: 0,
+        name: "Daniel Kim",
+        phone: "778-111-1111"
+      },
+      {
+        id: 1,
+        name: "Dohee Lee",
+        phone: "778-222-2222"
+      }
+    ]
   };
+
+  handleCreate = data => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    });
+  };
+
   render() {
+    const { information } = this.state;
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate} />
+        {JSON.stringify(information)}
       </div>
     );
   }
